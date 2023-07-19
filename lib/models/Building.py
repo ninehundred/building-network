19387ef38876e95bf10a91793e3952e851f1cd2d
OzyOz <oguzhanozkilinc@gmail.com>
2023-07-19 11:31:59 +0100

    OO - further refactoring

---
+++ lib/models/Building.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass, field
+from shapely import Polygon
+
+
+@dataclass
+class Room:
+    coordinates: []
+    centroid: []
+    number: str = ''
+    level: str = ''
+    polygon: Polygon = Polygon()
+    start: bool = False
+
+    # def __init__(self, coordinates, level, id):
+    #     self.coordinates = coordinates
+    #     self.level = level
+    #     self.id = id
+
+
+@dataclass
+class Door:
+    coordinates: []
+    centroid: list = field(default_factory=list)
+    upper_centroid: list = field(default_factory=list)
+    lower_centroid: list = field(default_factory=list)
+    id: str = ''
+    level: str = ''
+    rotation: float = 0.0
+    room: str = field(default_factory=set)
+    # identify if door leads to a stairwell
+    type: str = 'norm'
+    polygon: Polygon = Polygon()
+
+
+@dataclass
+class Floor:
+    Rooms: list[Room]
+    id: str = ''
+
+
+@dataclass
+class Building:
+    # floors should be a dict
+    # {"<floor_id>": dict(rooms)}
+    floors: list[Floor] = field(default_factory=set)
+    rooms: list[Room] = field(default_factory=list)
+    id: str = ''
+    doors: list[Door] = field(default_factory=list)
